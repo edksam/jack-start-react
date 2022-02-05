baf197b2945ed6c468a0cc27eb7a8c1535791a1c
Ebenezer Sam <ebenezer.sam@capgemini.com>
2022-02-05 13:48:57 +0000

    After Proptypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import propTypes from "prop-types";
+import pokemon from "./pokemon.json";
+
+const PokemonRow = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: propTypes.shape([]),
+  name: propTypes.shape({
+    english: propTypes.string,
+  }),
+  type: propTypes.arrayOf(propTypes.string),
+};
 
 function App() {
-  return <div className="App">Hello Now</div>;
+  return (
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.slice(0, 20).map((pokemon) => (
+            <PokemonRow
+              pokemon={pokemon}
+              key={[pokemon.id, pokemon.name.english].join(":")}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
